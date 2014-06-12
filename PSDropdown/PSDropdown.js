27d31cf114e2b9ff4959d4cd7adbc104eb8ae486
Paul Staff <paul@isprime-design.com>
2014-06-12 00:27:47 -0400

    Initial commit v0.9.1

---
+++ PSDropdown/PSDropdown.js
@@ -0,0 +1,90 @@
+  /*
+   *
+   * Programmer: Paul Staff
+   * Created: 2014.05.15
+   * Edited: 2014.06.11
+   *
+   * Copyright (c) 2014, nplexity, LLC.  All Rights Reserved.
+   * www.nplexity.com
+   *
+   * PSDropdown.js
+   *
+   * v 0.9.0
+   *
+   *
+   * This script converts all select elements with the class 'dropdown'
+   * to custom dropdown elements that can be fully styled with CSS.
+   *
+   *  Dropdown.js requires jQuery.  If jQuery is not included and/or the
+   *  user's browser does not support Javascript, all elements degrade
+   *  gracefully and continue to display as standard select elements.
+   *
+   */
+
+$(document).ready(function() {
+    window.PSDropdown = new PSDropdown;
+    PSDropdown.convert();
+});
+
+function PSDropdown() {
+
+    // This function replaces all selects with a dropdown div containing
+    // a readonly text input and an unordered list of all options
+    this.convert = function() {
+
+        // Loop through each select with the class 'dropdown'
+        $("select.dropdown").each(function() {
+
+            // Create a new dropdown DOM element
+            var dropdown = $('<div>', {
+                class: 'dropdown'
+            });
+
+            // Append the text input and unordered list
+            dropdown.append('<input type="text" class="' + $(this).attr('class') + '" id="' + $(this).attr('id') + '" value="' + $(this).children("option").first().html() + '" readonly /><ul></ul>');
+
+            // Create the options variable
+            var options = '';
+
+            // Populate the options variable by looping through select options
+            $(this).children("option").each(function() { options += '<li data-val="' + $(this).val() + '">' + $(this).html() + '</li>'; });
+
+            // Append the options to the unordered list
+            dropdown.children("ul").append(options);
+
+            // Replace the select element with the created dropdown element
+            $(this).replaceWith(dropdown);
+        });
+
+        // Execute the initialize function
+        initializeDropdowns();
+    }
+
+    // This function initializes all dropdowns
+    function initializeDropdowns() {
+
+        // Set the click event for each dropdown
+        $(".dropdown input[type='text']").click(function() {
+
+            // Set the list position below the text input and width equal to input width
+            $(this).siblings("ul").css({'top': $(this).outerHeight(true), 'left': 0, 'width': $(this).outerWidth()}).fadeIn(200);
+
+            // Set the click event for each list element
+            $(".dropdown ul li").click(function() {
+                $(this).parent().siblings("input[type='text']").val($(this).attr("data-val"));
+
+                // Remove click event to prevent duplicates
+                $(".dropdown ul li").off("click");
+            });
+
+            // Set document mouseup function to close the list
+            $(document).mouseup(function (e)
+            {
+                $(".dropdown ul").fadeOut(200);
+
+                // Remove click event to prevent duplicates
+                $(document).off('mouseup');
+            });
+        });
+    }
+}
---
+++ readme.md
@@ -0,0 +1,42 @@
+PSDropdown - v0.9.1
+==========
+
+PSDropdown provides an alternative to standard HTML select elements. It converts all select elements with the class `PSDropdown` to custom `PSDropdown` elements that consist of a readonly text element and a list of options. To use this javascript plugin, simply include the `PSDropdown.js` and `PSDropdown.css` files and add the `PSDropdown` class to all select elements.
+
+
+Installation
+------------
+
+
+### Prerequisites
+
+- jQuery 2.0+ is required to run all PSToolkit plugins
+
+### Instructions
+
+1. Download [PSDropdown.zip](http://paulstaff.com/random/PSToolkit/PSDropdown/PSDropdown.zip).
+2. Unzip the contents and include the `PSDropdown.js` and `PSDropdown.css` files in the plugins folder for your project.
+3. Include the following two lines in the `<head>` section of your HTML file (be sure to change file path):
+
+```
+	<script src="path/to/plugins/folder/PSDropdown.js"></script>
+	<link rel="stylesheet" href="path/to/plugins/folder/PSDropdown.css">
+```
+
+4. Add the class `dropdown` to all select elements that you would like to convert: 
+
+```
+	<select class="dropdown">
+		<option value="1">This is an Option</option>
+        <option value="2">Another Option</option>
+        <option value="3">Option the Third</option>
+    </select>           
+```
+
+5. Optionally, you can call `PSDropdown.convert()` at any point to re-render all select inputs on the page as dropdown inputs (use this after inserting a new select element in the DOM).  This does not affect select inputs already rendered as dropdowns.
+
+
+Developed By
+------------
+
+[Paul Staff](http://paulstaff.com)
